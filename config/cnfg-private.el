5fd89103747613994a89eac0e82c9d0c84e1dd0d
Ziga Stopinsek <sigi.kajzer@gmail.com>
2014-07-31 08:55:11 +0200

    more solid private loading

--- config/cnfg-private.el
+++ config/cnfg-private.el
@@ -1,7 +1,13 @@
-;; private vars:
-;; - my-nick
-;; - my-full-name
-(setq my-nick ""
-      my-full-name "")
-(if (eq system-type 'gnu/linux) (load-file (concat my-emacs-d "config/private.el.gpg")))
+(defun own/no-private () 
+  (setq my-nick ""
+		my-full-name "")
+  (setq my-org-docs-dir '("~"))
+  (setq helm-delicious-user ""
+		helm-delicious-password ""))
+
+(if (file-exists-p (concat my-emacs-d "config/private.el.gpg"))
+	(load-file (concat my-emacs-d "config/private.el.gpg"))
+  (own/no-private))
+
+
 (provide 'cnfg-private)
