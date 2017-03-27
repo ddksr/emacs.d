47ac9ba01eca1e1ee116a799ac2c019284ac6b47
Ziga Stopinsek <sigi.kajzer@gmail.com>
2017-03-27 13:19:07 +0200

    python =)

--- etc/packages.el
+++ etc/packages.el
@@ -24,7 +24,7 @@ Return a list of installed packages or nil for every skipped package."
  ;;;;;;;;; Basic
  'auto-compile ; auto compile elisp files
  'flx-ido ; improve IDO minibuffer mode
- 'key-chord ; enable two-key commands
+; 'key-chord ; enable two-key commands
  'undo-tree ; enable undo-tree
  'multiple-cursors ; enable multiple cursors
  'ace-jump-mode ; enable jumping around in buffer
@@ -35,7 +35,7 @@ Return a list of installed packages or nil for every skipped package."
  'ibuffer-vc ; organize ibuffer using version control
  'smex ; ido for elisp interactive functions
  'expand-region ; expand region (s-e)
- 'visual-regexp ; for better regexes
+; 'visual-regexp ; for better regexes
  ;'doremi ; change things incrementally 
  'paradox ; improved package management
  'hydra ; keybinding management
@@ -44,12 +44,12 @@ Return a list of installed packages or nil for every skipped package."
  'recentf ; recent projects and files
  'projectile ; simple project management
  'grizzl ; file completion system for projectile
-
+ 'flyspell-lazy ; check spelling
+ 
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  ;;;;;;;;; ORG-MODE (latest)
  'org
  'orglink
- 'org2blog
  'outorg
  'outshine
  
@@ -57,81 +57,32 @@ Return a list of installed packages or nil for every skipped package."
  ;;;;;;;;; HELM - search anything
  'helm ; default helm
  'helm-anything ; for compatibility for older packages
- ;; TODO: check and use this packages
- ;'helm-dash ; Helm search trough different docummentations
- ;'helm-flycheck
- ;'helm-flymake
- ;'helm-git
  'helm-projectile ; search all files in the current project
  'helm-pydoc ; search python documentation
  'helm-c-yasnippet ; yasnippet with helm
- 'helm-helm-commands ; list all helm commands
+
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  ;;;;;;;;; Text manipulation and search
- 'move-text ; move text up and down on buffer TODO: check if keybindings
- 'volatile-highlights ; highlight pastes etc.
+
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ ;;;;;;;;; Programming
+ 'yasnippet
+ 'flymake
+ 
+ ;; Python
+ 'python-mode
+ 'py-autopep8 ; peeeeeep
+ 'flymake-python-pyflakes
+ 'anaconda-mode ; semantic autocomplete
+ 'company-anaconda ; semantic autocomplete
 
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  ;;;;;;;;; Appearance
  'powerline ; make powerline more beautiful
  'git-gutter-fringe ; show git additions in deletions in buffer
  'rainbow-delimiters ; colorize delimiters (like brackets) for easier debugging
+ 'volatile-highlights ; highlight changes
  
- ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- ;;;;;;;;; Programming languages utilities
- 'git-timemachine ; move trough file git history
- 'git-messenger ; see line commit
- 'gist ; integration with GIST
- 'emr ; emacs refactoring lib
- 'yasnippet ; provide snippets for faster programming
- 'markdown-mode ; MARKDOWN mode
- 'es-mode ; Elasticsearch
- ;'pkgbuild-mode ; for creating packages with emacs
- 'php-mode ; PHP mode
- 'php-extras ; PHP mode utilities
- 'php-eldoc ; PHP documentation TODO: use docs
- 'virtualenvwrapper ; for python virtualenv manipulation
- 'py-autopep8 ; peeeeeep
- 'pyenv-mode
- 'anaconda-mode ; python code navigation
- ;'python-pylint ; check python syntax TODO: check if using this
- 'jedi ; python autocomplete
- 'js2-mode ; JAVASCRIPT mode
- 'js2-refactor ; refactoring lib for javascript
- 'tern ; javascript environment
- 'tern-auto-complete ; TERN javascript autocomplete
-; 'tj-mode ; tj-mode
- 'zencoding-mode ; code with ZEN (for creating html using selectors)
- 'web-mode ; for editing web files (html, css)
- 'ruby-mode ; RUBY mode
- 'inf-ruby ; ruby console
- 'ruby-refactor ; ruby refactoring lib
- 'ruby-electric ; faster ruby programming (autocompletion and closing)
-; 'rsense ; ruby development tools
- 'auto-complete ; autocomplete mode for various programming languages
-; 'company ; alternative for autocomplete - use for ac-company
- 'auto-complete-auctex ; latex autocomplete
- 'ac-js2 ; autocomplete javascript
- 'org-ac ; autocomplete for ORG mode TODO: set up
- 'ac-helm
- 'ac-c-headers
- 'ac-octave ; autocomplete for octave
- 'ac-anaconda ; autocomplete for anaconda
- 'flyspell-lazy ; check spelling
- 'flymake ; check syntax
- 'flymake-shell ; syntax checking for SH
- 'flymake-php ; syntax checking for PHP
- 'flymake-python-pyflakes ; use pyflakes via flymake for python
- 'flymake-ruby ; ruby syntax checking
- 'flymake-css ; check css syntax
- 'flymake-yaml ; yaml
- 'flymake-less
- 'go-mode
- 'go-autocomplete
- 'flymake-go
-
- 'yaml-mode
-
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  ;;;;;;;;; Themes
  ;'color-theme ; color theme helper package
@@ -141,14 +92,6 @@ Return a list of installed packages or nil for every skipped package."
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  ;;;;;;;;; Applications and their additions
  'magit ; git source control
- 'auctex ; latex editor
- 'latex-extra ; extra latex commands
- 'notifications ; for notifications via emacs
- 'howdoi ; get stack overflow answers trough emacs
- 'prodigy ; manage services trouhg emacs
- 'multi-term ; terminal emulator via emacs
- 'restclient ; RESTful emacs client
- 'hackernews
 )
 
 ;; Packages I used but didn't serve my needs:
