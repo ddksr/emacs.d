6b0dfc12fa33efbe53f2dbd5c71bf82aaa00c035
Žiga Stopinšek <sigi.kajzer@gmail.com>
2018-04-15 21:17:51 +0200

    ...

--- etc/custom.el
+++ etc/custom.el
@@ -6,23 +6,22 @@
  '(LaTeX-command "latex -synctex=1")
  '(custom-safe-themes
    (quote
-	("f0ea6118d1414b24c2e4babdc8e252707727e7b4ff2e791129f240a2b3093e32" default)))
- '(js2-basic-offset 4)
+    ("f0ea6118d1414b24c2e4babdc8e252707727e7b4ff2e791129f240a2b3093e32" default)))
  '(package-selected-packages
    (quote
-	(flymake-less less-css-mode zencoding-mode yaml-mode web-mode volatile-highlights visual-regexp virtualenvwrapper undo-tree tern-auto-complete smex ruby-refactor ruby-electric restclient rainbow-delimiters pyenv-mode py-autopep8 prodigy powerline php-extras php-eldoc pastels-on-dark-theme paradox outshine orglink org2blog org-linkany org-ac multi-term move-text markdown-mode magit latex-extra key-chord js2-refactor jedi inf-ruby ibuffer-vc howdoi helm-pydoc helm-projectile helm-helm-commands helm-c-yasnippet helm-anything hackernews grizzl go-mode go-autocomplete git-timemachine git-messenger git-gutter-fringe gist flyspell-lazy flymake-yaml flymake-shell flymake-ruby flymake-python-pyflakes flymake-php flymake-go flymake-css flx-ido expand-region es-mode emr auto-complete-auctex auto-compile afternoon-theme ace-window ace-jump-zap ace-jump-buffer ace-isearch ac-octave ac-js2 ac-helm ac-c-headers ac-anaconda)))
+    (php-mode jekyll-modes markdown-mode+ ranger company-tern flymake-jslint tern django-snippets json-mode js2-mode golint company-go go-snippets go-playground company-anaconda anaconda-mode flymake python-mode company-mode yasnippet-bundle fullscreen-mode elpy free-keys flymake-less less-css-mode zencoding-mode yaml-mode web-mode volatile-highlights visual-regexp virtualenvwrapper undo-tree tern-auto-complete smex ruby-refactor ruby-electric restclient rainbow-delimiters pyenv-mode py-autopep8 prodigy powerline php-extras php-eldoc pastels-on-dark-theme paradox outshine orglink org2blog org-linkany org-ac multi-term move-text markdown-mode magit latex-extra key-chord js2-refactor jedi inf-ruby ibuffer-vc howdoi helm-pydoc helm-projectile helm-helm-commands helm-c-yasnippet helm-anything hackernews grizzl go-mode go-autocomplete git-timemachine git-messenger git-gutter-fringe gist flyspell-lazy flymake-yaml flymake-shell flymake-ruby flymake-python-pyflakes flymake-php flymake-go flymake-css flx-ido expand-region es-mode emr auto-complete-auctex auto-compile afternoon-theme ace-window ace-jump-zap ace-jump-buffer ace-isearch ac-octave ac-js2 ac-helm ac-c-headers ac-anaconda)))
  '(paradox-automatically-star t)
  '(safe-local-variable-values
    (quote
-	((python-shell-completion-string-code . "';'.join(get_ipython().Completer.all_completions('''%s'''))
+    ((python-shell-completion-string-code . "';'.join(get_ipython().Completer.all_completions('''%s'''))
 ")
-	 (python-shell-completion-module-string-code . "';'.join(module_completion('''%s'''))
+     (python-shell-completion-module-string-code . "';'.join(module_completion('''%s'''))
 ")
-	 (python-shell-completion-setup-code . "from IPython.core.completerlib import module_completion")
-	 (python-shell-prompt-output-regexp . "Out\\[[0-9]+\\]: ")
-	 (python-shell-prompt-regexp . "In \\[[0-9]+\\]: ")
-	 (python-shell-interpreter-args . "/home/fgallina/Code/Projects/anue-site/anue/manage.py shell")
-	 (python-shell-interpreter . "python"))))
+     (python-shell-completion-setup-code . "from IPython.core.completerlib import module_completion")
+     (python-shell-prompt-output-regexp . "Out\\[[0-9]+\\]: ")
+     (python-shell-prompt-regexp . "In \\[[0-9]+\\]: ")
+     (python-shell-interpreter-args . "/home/fgallina/Code/Projects/anue-site/anue/manage.py shell")
+     (python-shell-interpreter . "python"))))
  '(send-mail-function (quote smtpmail-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
