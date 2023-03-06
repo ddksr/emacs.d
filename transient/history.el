2d06bb1855d27a7000041f59bade1d6ca8fb95a7
Ziga Stopinsek <sigi.kajzer@gmail.com>
2023-03-06 21:33:23 +0100

    various

--- transient/history.el
+++ transient/history.el
@@ -1,6 +1,11 @@
 ((magit-commit nil)
  (magit-log
+  (("--" "stats-tripper/app/Http/Controllers/Stats.php")
+   "--graph" "++header")
+  (("--" "stats-tripper/app/Http/Controllers/Stats.php")
+   "--color")
+  (("--" "stats-tripper/app/Http/Controllers/Stats.php")
+   "--graph")
   (("--" "stats-tripper/resources/js/pages/bucket-lists/View.vue")))
  (magit-rebase
-  ("--autostash" "--interactive")
-  ("--autostash")))
+  ("--autostash" "--interactive")))
