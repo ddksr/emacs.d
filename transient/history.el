ba461d953b53faac4ea831a4cda28911e8ac99ea
Žiga Stopinšek <sigi.kajzer@gmail.com>
2024-01-12 19:36:12 +0100

    ...

--- transient/history.el
+++ transient/history.el
@@ -1,11 +1,2 @@
-((magit-commit nil)
- (magit-log
-  (("--" "stats-tripper/app/Http/Controllers/Stats.php")
-   "--graph" "++header")
-  (("--" "stats-tripper/app/Http/Controllers/Stats.php")
-   "--color")
-  (("--" "stats-tripper/app/Http/Controllers/Stats.php")
-   "--graph")
-  (("--" "stats-tripper/resources/js/pages/bucket-lists/View.vue")))
- (magit-rebase
+((magit-rebase
   ("--autostash" "--interactive")))
